811f1086ff811db1ae25f15ac272f90b77ee37cc
Necessarius03 <anreo.me+github@gmail.com>
2025-04-19 15:46:21 +0700

    test

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -5,12 +5,10 @@ import { ConfigProvider } from 'antd';
 import viVN from 'antd/lib/locale/vi_VN';
 import App from './App';
 
-// Import styles
 import 'antd/dist/reset.css';
 import './styles/main.scss';
 import './index.css';
 
-// Cấu hình theme cho Ant Design
 const theme = {
   token: {
     colorPrimary: '#1677ff',
